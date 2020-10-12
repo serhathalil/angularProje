11aceb96005d4d31295e8cfab8b2c114f11b784a
Serhat Halil Silsüpür <serhatsilsupur@gmail.com>
2020-10-12 11:02:08 +0300

    navigasyon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { pathToFileURL } from 'url';
+import { HomeComponent } from './home/home.component';
+import { RoleComponent } from './role/role.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"home",
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+},
+{
+  path:"user",
+  component:UserComponent
+},
+{
+  path:"role",
+  component:RoleComponent
+},
+{
+  path:"user/:id",
+  component:UserDetailComponent
+},
+{
+  path:"**",
+  component:HomeComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
